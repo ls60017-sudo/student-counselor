4fb348f084c9994ed37c49b768b97ac845899e6f
Abdullah <ls60017@gmail.com>
2026-08-30 06:03:57 +0000

    تحديث 147: إصلاح اختفاء/رجوع التقارير الكبيرة بعد مزامنة السحابة

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "student-counselor-v0.40.15-beta-update146-taskdelete";
+const CACHE_NAME = "student-counselor-v0.40.15-beta-update147-reportsyncfix";
 const APP_SHELL = [
   "/",
   "/index.html",
